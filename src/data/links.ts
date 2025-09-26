68c12fdd56ec0c31edd34c6e992c297a5236c198
Nathan Tranquilla <n1tranquilla@gmail.com>
2025-09-26 08:47:19 -0300

    update links

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,5 +1,13 @@
 // src/data/links.ts
 export const INQUIRE_LINK = "https://book.usesession.com/i/_ji73Jr_e/inquire";
+export const NEWBORN_LINK = "https://book.usesession.com/t/jzCdVHVNT";
+export const MATERNITY_LINK = "https://book.usesession.com/t/m-PqP2slk";
+export const BRANDING_LINK = "https://book.usesession.com/t/zK_v5yOEo";
+export const HEADSHOTS_LINK = "https://book.usesession.com/t/MuL13nQbW";
+export const MILESTONES_LINK = "https://book.usesession.com/t/aOqs7teEf";
+export const FAMILY_LINK = "https://book.usesession.com/t/qSKGXDFg9";
+export const CAKE_SMASH_LINK = "https://book.usesession.com/t/v0bCgBueC";
+export const HOLIDAY_MINIS_LINK = "https://book.usesession.com/i/_ji73Jr_e";
 export const SUBSCRIBE_LINK =
   "https://magnoliacoastsphotography.us14.list-manage.com/subscribe?u=4ebfced223d835c48a891029f&id=92c1de656f";
 export const INSTAGRAM_LINK =
